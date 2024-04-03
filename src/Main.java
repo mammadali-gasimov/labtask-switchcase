8c5a2c688ec75551d5e5303cbf2c22132dfd8012
mammadali-gasimov <mammadali.gasimov@gmail.com>
2024-04-03 21:10:43 +0400

    add seond task

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter first number:");
-        int firstNumber = scanner.nextInt();
-        System.out.println("Enter second number:");
-        int secondNumber = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter operation sign:");
-        String operation = scanner.nextLine();
+        // ------- Task 1 --------
+//        System.out.println("Enter first number:");
+//        int firstNumber = scanner.nextInt();
+//        System.out.println("Enter second number:");
+//        int secondNumber = scanner.nextInt();
+//        scanner.nextLine();
+//        System.out.println("Enter operation sign:");
+//        String operation = scanner.nextLine();
 
-        switch(operation) {
-            case "*":
-                System.out.println(firstNumber * secondNumber);
-                break;
-            case "-":
-                System.out.println(firstNumber - secondNumber);
-                break;
-            case "+":
-                System.out.println(firstNumber + secondNumber);
-                break;
-            case "/":
-                System.out.println(firstNumber / secondNumber);
-                break;
-            case "%":
-                System.out.println(firstNumber % secondNumber);
-                break;
-            default:
-                System.out.println("False operation!");
+//        switch(operation) {
+//            case "*":
+//                System.out.println(firstNumber * secondNumber);
+//                break;
+//            case "-":
+//                System.out.println(firstNumber - secondNumber);
+//                break;
+//            case "+":
+//                System.out.println(firstNumber + secondNumber);
+//                break;
+//            case "/":
+//                System.out.println(firstNumber / secondNumber);
+//                break;
+//            case "%":
+//                System.out.println(firstNumber % secondNumber);
+//                break;
+//            default:
+//                System.out.println("False operation!");
+//        }
+
+
+        // ----- Task 2 -------
+        System.out.println("Enter the number of the month:");
+        int monthNumber = scanner.nextInt();
+        switch (monthNumber) {
+            case 12, 1, 2 -> System.out.println("Winter");
+            case 3, 4, 5 -> System.out.println("Spring");
+            case 6, 7, 8 -> System.out.println("Summer");
+            case 9, 10, 11 -> System.out.println("Autumn");
+            default -> System.out.println("Wrong month number!");
         }
     }
 }
